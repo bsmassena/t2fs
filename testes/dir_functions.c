919aa68ee3e63d42aed6c920ed5ec6c283ec205b
Pedro Perrone <psperrone@hotmail.com>
2018-11-12 10:09:58 -0200

    Increases dir functions tests

--- testes/dir_functions.c
+++ testes/dir_functions.c
@@ -6,28 +6,61 @@
 #include "../include/filesystem.h"
 
 int main() {
-  DIR2 dir;
+  DIR2 dir, other_dir;
   DIRENT2 entrada;
-  if(mkdir2("testDir") != 0) {
+  int i;
+  char names[][9] = { ".", "..", "testDir", "testFile" };
+
+  // Create dir to controll environment
+  if(mkdir2("directory") != 0) {
     printf("ERRO NA CRIACAO DO DIRETORIO DE TESTE.\n");
     exit(1);
   }
-  dir = opendir2("/");
+  // Create dir inside new dir
+  if(mkdir2("directory/testDir") != 0) {
+    printf("ERRO NA CRIACAO DO DIRETORIO DE TESTE.\n");
+    exit(1);
+  }
+  // Create file inside new dir
+  if(create2("directory/testFile") < 0) {
+    printf("ERRO NA CRIACAO DO ARQUIVO DE TESTE.\n");
+    exit(1);
+  }
+  // Open new dir
+  dir = opendir2("./directory");
   if(dir < 0) {
     printf("ERRO NA ABERTURA DO DIRETORIO DE TESTE.\n");
     exit(1);
   }
-  if(readdir2(dir, &entrada) != 0) {
-    printf("ERRO NA LEITURA DO DIRETORIO DE TESTE.\n");
-    exit(1);
+
+  // For each element inside new dir, compare name
+  for(i = 0; i <= 3; i++) {
+    if(readdir2(dir, &entrada) != 0) {
+      printf("ERRO NA LEITURA DO DIRETORIO DE TESTE.\n");
+      exit(1);
+    }
+    if(strcmp(entrada.name, names[i]) == 0) {
+      printf("Leitura correta da entrada %s.\n", entrada.name);
+    } else {
+      printf("ERRO NA LEITURA DO DIRETORIO.\n");
+    }
   }
-  if(strcmp(entrada.name, ".") == 0) {
-    printf("Leitura correta do diretorio.\n");
-  } else {
-    printf("ERRO NA LEITURA DO DIRETORIO.\n");
-  }
+
   if(closedir2(dir) < 0) {
     printf("ERRO AO FECHAR O DIRETORIO.\n");
   }
+
+  // Test other ways to open dir
+  // 1. Absolute
+  other_dir = opendir2("/directory");
+  if(other_dir != dir) {
+    printf("ERRO NA ABERTURA DE DIRETORIO COM CAMINHO ABSOLUTO.\n");
+  }
+  // 2. Without ./
+  other_dir = opendir2("directory");
+  if(other_dir != dir) {
+    printf("ERRO NA ABERTURA DE DIRETORIO COM CAMINHO RELATIVO SEM ./\n");
+  }
+  closedir2(other_dir);
   return 0;
 }
